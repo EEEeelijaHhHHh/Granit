27a91fb36698cc59f9f4c57ac87858a49278ecfa
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-09-11 07:33:47 +0300

    Added Smooth Scroll's Animation

--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@
     })
   });
 }());
+
+/* Anchor Smooth Scroll's Animation */
+(function () {
+  document.querySelectorAll('[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const link = document.querySelector(this.getAttribute('href'));
+      console.log(link);
+      link.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+}());
