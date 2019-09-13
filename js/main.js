81643f61f691fbd37ebfa8a1fe8cf76c140d5257
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-09-13 04:18:58 +0300

    Close menu if clicked out of menu

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
 /* Menu Hamburger */
-(function() {
+(function () {
   const menuBtn = document.querySelector('.header-nav__btn');
   const menu = document.querySelector('.header-nav');
   const links = document.querySelectorAll('.main-nav__link');
   const active = 'header-nav_active';
 
+  // Hamburger clicked
   menuBtn.addEventListener('click', () => {
     menu.classList.toggle(active);
   });
 
+  // Someone menu's links clicked
   links.forEach(link => {
     link.addEventListener('click', () => {
       menu.classList.toggle(active);
     })
   });
+
+
+  // Clicked out of menu
+  document.body.addEventListener('click', event => {
+    if (!menu.contains(event.target) && menu.className === `header-nav ${active}`) {
+      menu.classList.toggle(active);
+    }
+  });
 }());
+
 
 /* Anchor Smooth Scroll's Animation */
 (function () {
